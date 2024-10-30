a27af00e127a75bef376834dd1c816ce4815cc7d
meeyth <anurupbhowmick2003@gmail.com>
2024-10-30 22:32:03 +0530

    update liked-blog page

--- src/features/liked/LikedBlog.jsx
+++ src/features/liked/LikedBlog.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useGetMyLikedBlogsQuery } from './likedBlogApiSlice'
 import { Link } from 'react-router-dom';
+import BlogCard from '../../components/BlogCard';
 
 const LikedBlog = () => {
     const { data, } = useGetMyLikedBlogsQuery()
@@ -8,17 +9,8 @@ const LikedBlog = () => {
     return (
         <>
             <h1>LikedBlog</h1>
-            {data?.map(({ likedBlogs }) => (
-                <div key={likedBlogs._id} style={{ border: "1px solid white", marginBottom: 5, cursor: "pointer", height: "25rem" }}>
-                    <p>{likedBlogs._id}</p>
-                    <h2>{likedBlogs.title}</h2>
-                    <p>{likedBlogs.content}</p>
-                    <img src={likedBlogs.image} alt={likedBlogs.title} style={{ height: "10rem", width: "15rem", objectFit: "contain" }} />
-                    <br />
-                    <small>Written by: {likedBlogs.owner}</small>
-                    <br />
-                    <Link to={`/blogs/${likedBlogs._id}`}>Read More</Link>
-                </div>
+            {data?.map((blog) => (
+                <BlogCard key={blog._id} blog={blog} />
             ))}
         </>
     )
